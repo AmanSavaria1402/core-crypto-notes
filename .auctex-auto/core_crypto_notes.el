b27b242a71a2527c92f3fa5a352492ba66392346
amansavaria14@gmail.com <amansavaria14@gmail.com>
2026-06-30 00:17:53 -0500

    first commit

---
+++ .auctex-auto/core_crypto_notes.el
@@ -0,0 +1,28 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "core_crypto_notes"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("graphicx" "") ("hyperref" "") ("amsfonts" "") ("float" "") ("listings" "") ("geometry" "top=1in" "bottom=1in" "left=0.7in" "right=0.7in")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "graphicx"
+    "hyperref"
+    "amsfonts"
+    "float"
+    "listings"
+    "geometry"))
+ :latex)
+
